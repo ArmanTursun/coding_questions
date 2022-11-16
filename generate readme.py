5d03211794328dc852fa950f9ea77d099182b2fa
ArmanTursun <armantursun94@gmail.com>
2022-11-16 14:10:14 +0900

    Add Sort Array By Parity

--- generate readme.py
+++ generate readme.py
@@ -5,10 +5,10 @@
 #######################################
 #######################################
 hardest = 'Easy' #  Medium, Hard
-title = '1748. Sum of Unique Elements'
-companies = [3] ## '1. Amazon', '2. Apple', '3. Facebook', '4. Google', '5. Microsoft'
-url = 'https://leetcode.com/problems/sum-of-unique-elements/description/'
-topic = ['Array', "Hashmap"]
+title = '905. Sort Array By Parity'
+companies = [1, 3, 4, 5] ## '1. Amazon', '2. Apple', '3. Facebook', '4. Google', '5. Microsoft'
+url = 'https://leetcode.com/problems/sort-array-by-parity/description/'
+topic = ['Array', "Two Pointers"]
 related = ''
 #######################################
 #######################################
