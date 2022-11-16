397dac38f375bc6a8451e6c72382f39bdb515f30
ArmanTursun <armantursun94@gmail.com>
2022-11-16 14:41:43 +0900

    Add Logger Rate Limiter

--- generate readme.py
+++ generate readme.py
@@ -5,10 +5,10 @@
 #######################################
 #######################################
 hardest = 'Easy' #  Medium, Hard
-title = '905. Sort Array By Parity'
-companies = [1, 3, 4, 5] ## '1. Amazon', '2. Apple', '3. Facebook', '4. Google', '5. Microsoft'
-url = 'https://leetcode.com/problems/sort-array-by-parity/description/'
-topic = ['Array', "Two Pointers"]
+title = '359. Logger Rate Limiter'
+companies = [1, 2, 3, 4, 5] ## '1. Amazon', '2. Apple', '3. Facebook', '4. Google', '5. Microsoft'
+url = 'https://leetcode.com/problems/logger-rate-limiter/description/'
+topic = ['Hashmap']
 related = ''
 #######################################
 #######################################
