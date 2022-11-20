10b222191966068f77a4473daca20f6c212bcfe1
ArmanTursun <armantursun94@gmail.com>
2022-11-20 16:06:50 +0900

    Add Goat Latin

--- generate_readme.py
+++ generate_readme.py
@@ -5,10 +5,10 @@
 #######################################
 #######################################
 hardest = 'Easy' #  Medium, Hard
-title = "1925. Count Square Sum Triples"
-companies = [1, 3] ## '1. Amazon', '2. Apple', '3. Facebook', '4. Google', '5. Microsoft'
-url = 'https://leetcode.com/problems/count-square-sum-triples/description/'
-topic = ['Math']
+title = "824. Goat Latin"
+companies = [3] ## '1. Amazon', '2. Apple', '3. Facebook', '4. Google', '5. Microsoft'
+url = 'https://leetcode.com/problems/goat-latin/description/'
+topic = ['String']
 related = ''
 #######################################
 #######################################
